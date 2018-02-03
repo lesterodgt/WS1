370fe278950d9166c02045a05e25735d01c0ac3a
lesterodgt <lesterod@hotmail.com>
2018-02-02 23:06:04 -0600

    actualizacion con ws 2

---
+++ src/test/java/com/grupo2/TestWS2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo2;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+
+/**
+ *
+ * @author lesterod
+ */
+
+public class TestWS2 {
+    
+
+    public void testCaseOne(){
+        assertEquals(3, 3);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+   
+    
+}
